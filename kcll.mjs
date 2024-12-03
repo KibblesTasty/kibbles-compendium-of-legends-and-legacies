387656afef1742f919c9165daa9db0b033eb41fc
Joseph <JPMeehan5@Gmail.com>
2024-12-02 21:18:20 -0800

    Blood Magic spell property

--- kcll.mjs
+++ kcll.mjs
@@ -10,12 +10,18 @@ Hooks.once("init", () => {
     mysticTechnique: "KCLL.MysticTechnique"
   });
 
+  CONFIG.DND5E.itemProperties.bloodMagic = {
+    label: "KCLL.BloodMagic.Label",
+    abbreviation: "KCLL.BloodMagic.Abbreviation",
+    reference: "Compendium.kibbles-compendium-of-legends-and-legacies.kcll-journals.JournalEntry.NXibCQQFLkrAGE1i.JournalEntryPage.7fVrfxPp9OykJ5OS"
+  }
+  CONFIG.DND5E.validProperties.spell.add("bloodMagic");
 
   const dazed = {
-    label: 'KCLL.Dazed',
+    label: "KCLL.Dazed",
     icon: `${MODULE_PATH}/assets/icons/dazed.svg`,
-    _id: dnd5e.utils.staticID('dnd5edazed'),
-    reference: 'Compendium.kibbles-compendium-of-legends-and-legacies.kcll-journals.JournalEntry.NXibCQQFLkrAGE1i.JournalEntryPage.8t64sXFlxtCVP3b1',
+    _id: dnd5e.utils.staticID("dnd5edazed"),
+    reference: "Compendium.kibbles-compendium-of-legends-and-legacies.kcll-journals.JournalEntry.NXibCQQFLkrAGE1i.JournalEntryPage.8t64sXFlxtCVP3b1",
   }
 
   CONFIG.DND5E.conditionTypes.dazed = dazed;
